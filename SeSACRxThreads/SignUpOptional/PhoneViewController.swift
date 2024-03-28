d72176c750d8037b158013b52524c7cdd1b72976
Minchelin42 <mje0211@naver.com>
2024-03-29 00:33:35 +0900

    PhoneViewController 로직 구현

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,19 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    var phoneNum = Observable.just("010")
+    let validationLabel = UILabel()
+    var validationText = BehaviorSubject(value: "휴대폰번호를 입력해주세요")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +28,43 @@ class PhoneViewController: UIViewController {
         
         configureLayout()
         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        phoneNum.bind(to: phoneTextField.rx.text).disposed(by: disposeBag)
+        
+        validationText.bind(to: validationLabel.rx.text).disposed(by: disposeBag)
+        
+        let validation = phoneTextField.rx.text.orEmpty
+        
+        validation.bind(with: self) { owner, value in
+            guard let number = Int(value) else {
+                owner.validationText.onNext("입력형식이 올바르지않습니다")
+                owner.validationLabel.isHidden = false
+                owner.nextButton.isEnabled = false
+                return
+            }
+            
+            if value.count < 10 {
+                owner.validationText.onNext("휴대폰번호는 10자이상으로 입력해주세요")
+                owner.validationLabel.isHidden = false
+                owner.nextButton.isEnabled = false
+            } else {
+                owner.validationLabel.isHidden = true
+                owner.nextButton.isEnabled = true
+            }
+            
+        }
+        .disposed(by: disposeBag)
+        
+        nextButton.rx.tap.bind(with: self) { owner, _ in
+            owner.navigationController?.pushViewController(NicknameViewController(), animated: true)
+        }.disposed(by: disposeBag)
+        
     }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
-    }
+
 
     
     func configureLayout() {
         view.addSubview(phoneTextField)
+        view.addSubview(validationLabel)
         view.addSubview(nextButton)
          
         phoneTextField.snp.makeConstraints { make in
@@ -38,9 +73,15 @@ class PhoneViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
+        validationLabel.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+            make.top.equalTo(validationLabel.snp.bottom).offset(30)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
